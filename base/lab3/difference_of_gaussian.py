a60eeaa171ec6539dff6e12ac7f02c4795c44779
Azen0n <jack.2013.ghost@gmail.com>
2022-04-18 18:45:42 +0500

    Add DoG implementation

--- base/lab3/difference_of_gaussian.py
+++ base/lab3/difference_of_gaussian.py
@@ -1,7 +1,19 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 from base.lab2.blurring import gaussian_filter
 
 
-def difference_of_gaussian(image: Image, sigma: float) -> Image:
+def difference_of_gaussian(image: Image, coefficient: float, sigma: float) -> Image:
     """2.2. Метод DoG для распознавания границ."""
-    pass
+    higher = gaussian_filter(image, coefficient * sigma)
+    lower = gaussian_filter(image, sigma)
+    higher_pixels = higher.load()
+    lower_pixels = lower.load()
+
+    new_image = Image.new('L', image.size)
+    draw = ImageDraw.Draw(new_image)
+
+    for i in range(image.width):
+        for j in range(image.height):
+            draw.point((i, j), 5 * (higher_pixels[i, j] - lower_pixels[i, j]))
+
+    return new_image
